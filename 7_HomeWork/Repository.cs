21b6db4ab8b34b6572f5762d4c49bae8d32b59e5
Kirill Vasilenko <kirvasilenko17@icloud.com>
2022-11-01 22:21:04 +1000

    2-th commit

---
+++ 7_HomeWork/Repository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _7_HomeWork
+{
+    class Repository
+    {
+        public Worker[] GetAllWorkers()
+        {
+
+        }
+
+        public Worker GetWorkerById(int id)
+        {
+
+        }
+
+        public void DeleteWorker(int id)
+        {
+
+        }
+
+        public void AddWorker(Worker worker)
+        {
+
+        }
+
+        public Worker[] GetWorkersBetweenTwoDates(DateTime dateFrom, DateTime dateTo)
+        {
+
+        }
+    }
+}
---
+++ 7_HomeWork/Worker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _7_HomeWork
+{
+    struct Worker
+    {
+        /// <summary>
+        /// ID сотрудника
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Дата и время добавления записи
+        /// </summary>
+        public DateTime DateTime { get; set; }
+
+        /// <summary>
+        /// Фамилия, имя, отчество сотрудника
+        /// </summary>
+        public string FIO { get; set; }
+
+        /// <summary>
+        /// Возраст сотрудника
+        /// </summary>
+        public int Age { get; set; }
+
+        /// <summary>
+        /// Рост сотрудника
+        /// </summary>
+        public int Height { get; set; }
+
+        /// <summary>
+        /// Дата рождения сотрудника
+        /// </summary>
+        public DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// Место рождения сотрудника
+        /// </summary>
+        public string PlaceOfBirth { get; set; }
+
+    }
+}
